efde58ee27a24cd9245f2ded5701bd0811e85c8c
wly <wlywly0735@126.com>
2023-11-20 17:11:18 +0800

    shortLink_module_短连接后管，token过滤路径

--- admin/src/main/java/com/wly/shortlinkStudy/admin/common/biz/user/UserTransmitFilter.java
+++ admin/src/main/java/com/wly/shortlinkStudy/admin/common/biz/user/UserTransmitFilter.java
@@ -1,11 +1,14 @@
 package com.wly.shortlinkStudy.admin.common.biz.user;
 
 import com.alibaba.fastjson2.JSON;
+import com.google.common.collect.Lists;
 import jakarta.servlet.*;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.StringRedisTemplate;
+
 import java.io.IOException;
+import java.util.List;
 
 
 /**
@@ -15,6 +18,11 @@ import java.io.IOException;
 public class UserTransmitFilter implements Filter {
 
     private final StringRedisTemplate stringRedisTemplate;
+
+    private static final List<String> IGNORE_URI = Lists.newArrayList(
+            "/api/short-link/v1/user/login",
+            "/api/short-link/v1/user/has-username"
+    );
 
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
