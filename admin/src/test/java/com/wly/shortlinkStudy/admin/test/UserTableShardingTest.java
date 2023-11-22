fd51c72244657046218eed0f78678b5824618df3
wly <wangleyan0735@163.com>
2023-11-22 16:15:41 +0800

    分库分表sql

--- admin/src/test/java/com/wly/shortlinkStudy/admin/test/UserTableShardingTest.java
+++ admin/src/test/java/com/wly/shortlinkStudy/admin/test/UserTableShardingTest.java
@@ -2,20 +2,25 @@ package com.wly.shortlinkStudy.admin.test;
 
 public class UserTableShardingTest {
 
-    public static final String SQL = "CREATE TABLE `t_user_%d`  (\n" +
-            "  `id` bigint NOT NULL AUTO_INCREMENT COMMENT 'ID',\n" +
-            "  `username` varchar(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '用户名',\n" +
-            "  `password` varchar(512) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '密码',\n" +
-            "  `real_name` varchar(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '真实姓名',\n" +
-            "  `phone` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '手机号',\n" +
-            "  `mail` varchar(512) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '邮箱',\n" +
-            "  `deletion_time` bigint NULL DEFAULT NULL COMMENT '注销时间戳',\n" +
-            "  `create_time` datetime NULL DEFAULT NULL COMMENT '创建时间',\n" +
-            "  `update_time` datetime NULL DEFAULT NULL COMMENT '修改时间',\n" +
-            "  `del_flag` tinyint(1) NULL DEFAULT NULL COMMENT '删除标识 0：未删除 1：已删除',\n" +
-            "  PRIMARY KEY (`id`) USING BTREE,\n" +
-            "  UNIQUE INDEX `idx_unique_username`(`username` ASC) USING BTREE\n" +
-            ") ENGINE = InnoDB AUTO_INCREMENT = 1551155203 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;";
+    public static final String SQL = "CREATE TABLE `t_link_%d` (\n" +
+            "  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'ID',\n" +
+            "  `domain` varchar(128) DEFAULT NULL COMMENT '域名',\n" +
+            "  `short_uri` varchar(8) DEFAULT NULL COMMENT '短链接',\n" +
+            "  `full_short_url` varchar(128) DEFAULT NULL COMMENT '完整短链接',\n" +
+            "  `origin_url` varchar(1024) DEFAULT NULL COMMENT '原始链接',\n" +
+            "\t`click_num` int(11) DEFAULT 0 COMMENT '点击量',\n" +
+            "\t`gid` varchar(32) DEFAULT NULL COMMENT '分组标识',\n" +
+            "\t`enable_status` tinyint(1) DEFAULT NULL COMMENT '启用标识 0：未启用 1：已启用',\n" +
+            "\t`created_type` tinyint(1) DEFAULT NULL COMMENT '创建类型 0：控制台 1：接口',\n" +
+            "\t`valid_date_type` tinyint(1) DEFAULT NULL COMMENT '有效期类型 0：永久有效 1：用户自定义',\n" +
+            "\t`valid_date` datetime DEFAULT NULL COMMENT '有效期',\n" +
+            "\t`describe` varchar(1024) DEFAULT NULL COMMENT '描述',\n" +
+            "  `create_time` datetime DEFAULT NULL COMMENT '创建时间',\n" +
+            "  `update_time` datetime DEFAULT NULL COMMENT '修改时间',\n" +
+            "  `del_flag` tinyint(1) DEFAULT NULL COMMENT '删除标识 0：未删除 1：已删除',\n" +
+            "  PRIMARY KEY (`id`),\n" +
+            "  UNIQUE KEY `idx_unique_full_short_uri` (`full_short_url`) USING BTREE\n" +
+            ") ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;";
     public static void main(String[] args) {
         for (int i = 0; i < 16; i++) {
             System.out.printf((SQL) + "%n", i) ;
