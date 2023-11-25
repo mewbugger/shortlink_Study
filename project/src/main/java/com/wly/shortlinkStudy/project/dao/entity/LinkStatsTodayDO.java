bf030694eca51d2ef99368f4a095af10231fa383
wly <wangleyan0735@163.com>
2023-11-25 22:41:19 +0800

    开发短链接分页监控相关功能

---
+++ project/src/main/java/com/wly/shortlinkStudy/project/dao/entity/LinkStatsTodayDO.java
@@ -0,0 +1,55 @@
+package com.wly.shortlinkStudy.project.dao.entity;
+
+import com.baomidou.mybatisplus.annotation.TableName;
+import com.wly.shortlinkStudy.project.common.database.BaseDO;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+/**
+ * 短链接今日统计实体
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@TableName("t_link_stats_today")
+public class LinkStatsTodayDO extends BaseDO {
+    /**
+     * id
+     */
+    private Long id;
+
+    /**
+     * 分组标识
+     */
+    private String gid;
+
+    /**
+     * 短链接
+     */
+    private String fullShortUrl;
+
+    /**
+     * 日期
+     */
+    private Date date;
+
+    /**
+     * 今日pv
+     */
+    private Integer todayPv;
+
+    /**
+     * 今日uv
+     */
+    private Integer todayUv;
+
+    /**
+     * 今日ip数
+     */
+    private Integer todayUip;
+}
---
+++ project/src/main/java/com/wly/shortlinkStudy/project/dao/mapper/LinkStatsTodayMapper.java
@@ -0,0 +1,10 @@
+package com.wly.shortlinkStudy.project.dao.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.wly.shortlinkStudy.project.dao.entity.LinkStatsTodayDO;
+
+/**
+ * 短链接今日统计持久层
+ */
+public interface LinkStatsTodayMapper extends BaseMapper<LinkStatsTodayDO> {
+}
