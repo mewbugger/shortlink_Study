536a9a7180219c8f22ec4f3aee9b36e99f93b009
wly <wangleyan0735@163.com>
2023-12-08 19:27:33 +0800

    优化：单个用户创建分组数量不能超过20

--- admin/src/main/java/com/wly/shortlinkStudy/admin/service/impl/GroupServiceImpl.java
+++ admin/src/main/java/com/wly/shortlinkStudy/admin/service/impl/GroupServiceImpl.java
@@ -1,11 +1,14 @@
 package com.wly.shortlinkStudy.admin.service.impl;
 
 import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.core.collection.CollUtil;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.conditions.update.LambdaUpdateWrapper;
 import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.wly.shortlinkStudy.admin.common.biz.user.UserContext;
+import com.wly.shortlinkStudy.admin.common.constant.RedisCacheConstant;
+import com.wly.shortlinkStudy.admin.common.convention.exception.ClientException;
 import com.wly.shortlinkStudy.admin.common.convention.result.Result;
 import com.wly.shortlinkStudy.admin.dao.entity.GroupDO;
 import com.wly.shortlinkStudy.admin.dao.mapper.GroupMapper;
@@ -16,7 +19,11 @@ import com.wly.shortlinkStudy.admin.remote.dto.ShortLinkRemoteService;
 import com.wly.shortlinkStudy.admin.remote.dto.resp.ShortLinkGroupCountQueryRespDTO;
 import com.wly.shortlinkStudy.admin.service.GroupService;
 import com.wly.shortlinkStudy.admin.toolkit.RandomGenerator;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -28,7 +35,13 @@ import java.util.Optional;
  */
 @Slf4j
 @Service
+@RequiredArgsConstructor
 public class GroupServiceImpl extends ServiceImpl<GroupMapper, GroupDO> implements GroupService {
+
+    private final RedissonClient redissonClient;
+
+    @Value("${short-link.group.max-num}")
+    private Integer groupMaxNum;
 
     /**
      * 后续重构为 SpringCloud Feign 调用
@@ -43,22 +56,36 @@ public class GroupServiceImpl extends ServiceImpl<GroupMapper, GroupDO> implemen
 
     @Override
     public void savaGroup(String username, String groupName) {
-        String gid;
-        // 反复生成随机的gid直到gid不重复
-        while (true) {
-            gid = RandomGenerator.generateRandom();
-            if (hasGid(username, gid)) {
-                break;
+        RLock lock = redissonClient.getLock(String.format(RedisCacheConstant.LOCK_GROUP_CREATE_KEY, username));
+        lock.lock();
+        try {
+            LambdaQueryWrapper<GroupDO> queryWrapper = Wrappers.lambdaQuery(GroupDO.class)
+                    .eq(GroupDO::getUsername, username)
+                    .eq(GroupDO::getDelFlag, 0);
+            List<GroupDO> groupDOList = baseMapper.selectList(queryWrapper);
+            if (CollUtil.isNotEmpty(groupDOList) && groupDOList.size() == groupMaxNum) {
+                throw new ClientException(String.format("已超出最大分组数:%d", groupMaxNum));
             }
-        }
-        GroupDO groupDO = GroupDO.builder()
-                .gid(gid)
-                .sortOrder(0)
-                .username(username)
-                .name(groupName)
-                .build();
+            String gid;
+            // 反复生成随机的gid直到gid不重复
+            while (true) {
+                gid = RandomGenerator.generateRandom();
+                if (hasGid(username, gid)) {
+                    break;
+                }
+            }
+            GroupDO groupDO = GroupDO.builder()
+                    .gid(gid)
+                    .sortOrder(0)
+                    .username(username)
+                    .name(groupName)
+                    .build();
 
-        baseMapper.insert(groupDO);
+            baseMapper.insert(groupDO);
+        } finally {
+            lock.unlock();
+        }
+
     }
 
     @Override
