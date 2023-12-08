30886fa095d57236ef1981209c7f4d2656f490ea
wly <wangleyan0735@163.com>
2023-12-08 18:11:42 +0800

    修复短链接从删除恢复后跳转空指针恢复问题

--- project/src/main/java/com/wly/shortlinkStudy/project/service/impl/ShortLinkServiceImpl.java
+++ project/src/main/java/com/wly/shortlinkStudy/project/service/impl/ShortLinkServiceImpl.java
@@ -214,7 +214,8 @@ public class ShortLinkServiceImpl extends ServiceImpl<ShortLinkMapper, ShortLink
                     .eq(ShortLinkDO::getEnableStatus, 0);
             ShortLinkDO shortLinkDO = baseMapper.selectOne(queryWrapper);
             //短链接不存在或者当前短链接的有效期已经过期了
-            if (shortLinkDO == null || shortLinkDO.getValidDate().before(new Date())) {
+            //还需要判断validDate不为空，因为如果是永久有效的话，删除后再复原的话是没有有效期的,即是空，这时就会跳转到404
+            if (shortLinkDO == null || shortLinkDO.getValidDate() != null && shortLinkDO.getValidDate().before(new Date())) {
                 stringRedisTemplate.opsForValue().set(String.format(RedisKeyConstant.GOTO_IS_NULL_SHORT_LINK_KEY, fullShortUrl), "-", 30, TimeUnit.MINUTES);
                 ((HttpServletResponse) response).sendRedirect("/page/notfound");
                 return;
